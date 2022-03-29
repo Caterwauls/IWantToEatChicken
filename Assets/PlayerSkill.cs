36f556175befc41afe821004761803954bbfd29a
Caterwauls <apocalypse10@naver.com>
2022-03-29 16:31:19 +0900

    Complite PlayerSkill

--- Assets/PlayerSkill.cs
+++ Assets/PlayerSkill.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 public class PlayerSkill : MonoBehaviour
 {
 
@@ -11,6 +12,10 @@ public class PlayerSkill : MonoBehaviour
     public float stoppingTime = 2f;
     public List<Cube> enemys;
     public AudioSource music;
+    public AudioSource tiktak;
+    public PostProcessVolume stopEffect;
+    public float x = 0;
+
 
 
     private PlayerMove playerMove;
@@ -39,6 +44,8 @@ public class PlayerSkill : MonoBehaviour
             enemys[i].enabled = true;
         }
         shockwave.Stop();
+        tiktak.Stop();
+        stopEffect.profile.GetSetting<Bloom>().active = false;
         timeStopStart = false;
         toggle = true;
     }
@@ -54,8 +61,11 @@ public class PlayerSkill : MonoBehaviour
 
     public void timeStop()
     {
+        tiktak.pitch = 0.85f;
         timeStopStart = true;
+        stopEffect.profile.GetSetting<Bloom>().active = true;
         shockwave.Play();
+        tiktak.Play();
         StartCoroutine(CoroutineCooldownTimer());
         for (int i = 0; i < enemys.Count; i++)
         {
@@ -76,19 +86,23 @@ public class PlayerSkill : MonoBehaviour
         if (timeStopStart)
         {
             pitchDown();
+            StopEffect();
 
         }
         else if (toggle)
         {
             pitchUp();
+            StartEffectAfterStop();
         }
+        tiktakPicth();
+
     }
 
     private void pitchDown()
     {
         if (music.pitch > 0)
         {
-            music.pitch -= Time.deltaTime;
+            music.pitch -= 0.8f * Time.deltaTime;
         }
     }
 
@@ -105,6 +119,37 @@ public class PlayerSkill : MonoBehaviour
         }
     }
 
+    private void tiktakPicth()
+    {
+        if(tiktak.pitch > 0)
+        {
+            tiktak.pitch -= 0.33f * Time.deltaTime;
+        }
+    }
+
+    private void StopEffect()
+    {
+
+        
+        x += 1.5f * Time.deltaTime;
+        if (x >= 0.7)
+        {
+            x = 0.75f;
+            return;
+        }
+        stopEffect.profile.GetSetting<Vignette>().intensity.Override(x);
+    }
+
+    private void StartEffectAfterStop()
+    {
+        x -= 1.5f * Time.deltaTime;
+        if (x <= 0)
+        {
+            x = 0;
+            return;
+        }
+        stopEffect.profile.GetSetting<Vignette>().intensity.Override(x);
+    }
 
 
 }
