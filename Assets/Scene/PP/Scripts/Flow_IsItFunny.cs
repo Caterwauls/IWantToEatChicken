89bb54b70f187d57f5349c7ce4371ca155d39b65
Caterwauls <apocalypse10@naver.com>
2022-06-24 05:51:25 +0900

    debugOn 제거

--- Assets/Scene/PP/Scripts/Flow_IsItFunny.cs
+++ Assets/Scene/PP/Scripts/Flow_IsItFunny.cs
@@ -15,9 +15,6 @@ public class Flow_IsItFunny : Flow
     public SpacePressEffectOn spaceEffect;
     public GameObject cutSceneDirector;
 
-    public bool debugOn;
-
-
     protected override IEnumerator FlowRoutine()
     {
         PlayerControl pc = player.GetComponent<PlayerControl>();
@@ -28,105 +25,98 @@ public class Flow_IsItFunny : Flow
 
         yield return StopTimeAnimatedRoutine();
 
+        yield return new WaitForSecondsRealtime(1.5f);
 
+        yield return PrintDialogRoutine("재밌나요");
+        yield return AskChoiceRoutine("재밌나요");
 
-
-        if (!debugOn)
+        if (lastChoice == 0)
         {
+            yield return PrintDialogRoutine("아닌거같은데");
+        }
 
-            yield return new WaitForSecondsRealtime(1.5f);
 
-            yield return PrintDialogRoutine("재밌나요");
-            yield return AskChoiceRoutine("재밌나요");
+        yield return PrintDialogRoutine("뭐가불만인가요");
+        yield return AskChoiceRoutine("뭐가불만인가요");
+
+        yield return PrintDialogRoutine("그렇게보이진않는데");
+
+        otherCamera.SetActive(true);
+
+        Time.timeScale = 1;
+        otherPingPong.SetActive(true);
+
+        yield return new WaitForSecondsRealtime(4f);
+
+        yield return PrintDialogRoutine("다른사람들도똑같아");
+
+        otherCamera.SetActive(false);
+
+        yield return new WaitForSecondsRealtime(2f);
+
+        yield return StopTimeAnimatedRoutine();
+
+        yield return new WaitForSecondsRealtime(1.5f);
+
+        yield return PrintDialogRoutine("다들하는대로해라");
+        Time.timeScale = 1;
+
+        playerCloseupCam.SetActive(false);
+        yield return new WaitForSecondsRealtime(2f);
+
+        yield return new WaitUntil(() =>
+            Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return));
+
+        resistMessage.SetActive(true);
+
+        pc.isCanResist = true;
+        spaceEffect.canPlayEffect = true;
+        yield return new WaitUntil(() => pc.resistNum >= 10);
+
+        resistMessage.SetActive(false);
+        spaceEffect.canPlayEffect = false;
+        playerCloseupCam.SetActive(true);
+
+        yield return StopTimeAnimatedRoutine();
+        yield return new WaitForSecondsRealtime(1.5f);
+
+        pc.isCanResist = false;
+
+        yield return PrintDialogRoutine("방해하지마");
+        yield return AskChoiceRoutine("방해하지마");
+
+        yield return new WaitForSecondsRealtime(2f);
+
+        yield return PrintDialogRoutine("탈출하고싶나요");
+        yield return AskChoiceRoutine("탈출하고싶나요");
+
+        if (lastChoice == 0)
+        {
+            yield return PrintDialogRoutine("탈출하고싶다");
+        }
+        else if (lastChoice == 1)
+        {
+            yield return PrintDialogRoutine("탈출하고싶지않다");
+            yield return AskChoiceRoutine("탈출하고싶지않다");
 
             if (lastChoice == 0)
             {
-                yield return PrintDialogRoutine("아닌거같은데");
-            }
+                yield return PrintDialogRoutine("탈출하고싶지않다1");
+                playerCloseupCam.SetActive(false);
+                pc.isCanResist = false;
+                Time.timeScale = 1;
+                DialogManager.instance.blurEffect.SetActive(true);
+                EndMessage.SetActive(true);
+                yield return new WaitUntil(() => (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space)));
 
+                restartMessage.SetActive(true);
+                yield break;
 
-            yield return PrintDialogRoutine("뭐가불만인가요");
-            yield return AskChoiceRoutine("뭐가불만인가요");
-
-            yield return PrintDialogRoutine("그렇게보이진않는데");
-
-            otherCamera.SetActive(true);
-
-            Time.timeScale = 1;
-            otherPingPong.SetActive(true);
-
-            yield return new WaitForSecondsRealtime(4f);
-
-            yield return PrintDialogRoutine("다른사람들도똑같아");
-
-            otherCamera.SetActive(false);
-
-            yield return new WaitForSecondsRealtime(2f);
-
-            yield return StopTimeAnimatedRoutine();
-
-            yield return new WaitForSecondsRealtime(1.5f);
-
-            yield return PrintDialogRoutine("다들하는대로해라");
-            Time.timeScale = 1;
-
-            playerCloseupCam.SetActive(false);
-            yield return new WaitForSecondsRealtime(2f);
-
-            yield return new WaitUntil(() =>
-                Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return));
-
-            resistMessage.SetActive(true);
-            
-            pc.isCanResist = true;
-            spaceEffect.canPlayEffect = true;
-            yield return new WaitUntil(() => pc.resistNum >= 10);
-
-            resistMessage.SetActive(false);
-            spaceEffect.canPlayEffect = false;
-            playerCloseupCam.SetActive(true);
-
-            yield return StopTimeAnimatedRoutine();
-            yield return new WaitForSecondsRealtime(1.5f);
-
-            pc.isCanResist = false;
-
-            yield return PrintDialogRoutine("방해하지마");
-            yield return AskChoiceRoutine("방해하지마");
-
-            yield return new WaitForSecondsRealtime(2f);
-
-            yield return PrintDialogRoutine("탈출하고싶나요");
-            yield return AskChoiceRoutine("탈출하고싶나요");
-
-            if (lastChoice == 0)
-            {
-                yield return PrintDialogRoutine("탈출하고싶다");
             }
             else if (lastChoice == 1)
             {
-                yield return PrintDialogRoutine("탈출하고싶지않다");
-                yield return AskChoiceRoutine("탈출하고싶지않다");
-
-                if (lastChoice == 0)
-                {
-                    yield return PrintDialogRoutine("탈출하고싶지않다1");
-                    playerCloseupCam.SetActive(false);
-                    pc.isCanResist = false;
-                    Time.timeScale = 1;
-                    DialogManager.instance.blurEffect.SetActive(true);
-                    EndMessage.SetActive(true);
-                    yield return new WaitUntil(() => (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space)));
-
-                    restartMessage.SetActive(true);
-                    yield break;
-
-                }
-                else if (lastChoice == 1)
-                {
-                    yield return PrintDialogRoutine("탈출하고싶지않다2");
-                    yield return new WaitForSecondsRealtime(1f);
-                }
+                yield return PrintDialogRoutine("탈출하고싶지않다2");
+                yield return new WaitForSecondsRealtime(1f);
             }
         }
 
