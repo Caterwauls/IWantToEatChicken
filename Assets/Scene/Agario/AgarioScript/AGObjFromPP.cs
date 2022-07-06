17b0cee4c0dbc9a45a9c0b672413d2adfb2a1623
Eunseop Shim <lumeriith@gmail.com>
2022-07-07 05:15:56 +0900

    아가리오 메뉴 씬 연출 추가

---
+++ Assets/Scene/Agario/AgarioScript/AGObjFromPP.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AGObjFromPP : MonoBehaviour
+{
+    public GameObject effect;
+    public Vector3 angular;
+    private float _lastHitTime = float.NegativeInfinity;
+
+    private void Awake()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = angular;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (Time.unscaledTime - _lastHitTime < 10000f) return;
+        _lastHitTime = Time.unscaledTime;
+        Instantiate(effect, transform.position, Quaternion.identity);
+    }
+}
